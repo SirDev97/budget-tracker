1866e2a5dd472efbda3cd3e2041866dd67b42df6
Alkin Maystorov <alkin971@gmail.com>
2022-01-17 20:35:06 +0100

    Create Budget Context.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetsProvider } from './contexts/BudgetContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BudgetsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BudgetsProvider>,
   document.getElementById('root')
 );
