0b6adbb62cee0734d85f723dd3211347d24d07e1
Alkin Maystorov <alkin971@gmail.com>
2022-01-17 18:02:02 +0100

    Create BudgetCard component. Add util func for formatting currency.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
 import { Stack, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import BudgetCard from './components/BudgetCard';
 
 const App = () => {
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-          gap: '1rem',
-          alignItems: 'flex-start',
-        }}></div>
-    </Container>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+            gap: '1rem',
+            alignItems: 'flex-start',
+          }}>
+          <BudgetCard gray name="food" amount={500} max={500} />
+        </div>
+      </Container>
+    </>
   );
 };
 
