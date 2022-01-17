28dbb87d2f4752203f0db1744a21dc47d29b0355
Alkin Maystorov <alkin971@gmail.com>
2022-01-17 17:18:29 +0100

    Initial layout setup.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Stack, Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}></div>
+    </Container>
   );
 };
 
